ace5fc7269c0d334503644979cb391270b494a05
Woissil <amotion131@gmail.com>
2024-06-08 14:18:09 +0200

    useless push

---
+++ actors/elecloud/geo_header.h
@@ -0,0 +1,8 @@
+extern const GeoLayout elecloud_geo[];
+extern u8 elecloud_Material32_ci8[];
+extern u8 elecloud_Material32_pal_rgba16[];
+extern Vtx elecloud_Mball_006_mesh_layer_1_vtx_0[185];
+extern Gfx elecloud_Mball_006_mesh_layer_1_tri_0[];
+extern Gfx mat_elecloud_f3dlite_material[];
+extern Gfx mat_revert_elecloud_f3dlite_material[];
+extern Gfx elecloud_Mball_006_mesh_layer_1[];
