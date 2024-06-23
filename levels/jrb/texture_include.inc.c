af9d28c0c7f562ddac6e5c22427c3b8733511a8a
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-23 20:59:20 +0330

    pretty real stuff

--- levels/jrb/texture_include.inc.c
+++
@@ -1,5 +0,0 @@
-Gfx jrb_dl_PC_Computer___Geometry_Dash___Ground_1_i8_aligner[] = {gsSPEndDisplayList()};
-u8 jrb_dl_PC_Computer___Geometry_Dash___Ground_1_i8[] = {
-	#include "levels/jrb/PC_Computer_-_Geometry_Dash_-_Ground_1.i8.inc.c"
-};
-
