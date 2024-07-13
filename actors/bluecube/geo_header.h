4c595f163dbc576e3946262c10cd568e93b57979
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-07-13 23:11:35 +0330

    Broken blue

--- actors/bluecube/geo_header.h
+++ actors/bluecube/geo_header.h
@@ -1,7 +1,9 @@
 extern const GeoLayout bluecube_geo[];
 extern Lights1 bluecube_f3dlite_material_001_lights;
-extern u8 bluecube_m_grass_b1_ia16[];
+extern u8 bluecube_ccm_textures_08800_rgba16_ci8[];
+extern u8 bluecube_ccm_textures_08800_rgba16_pal_rgba16[];
 extern Vtx bluecube_Cube_mesh_layer_1_vtx_0[46];
 extern Gfx bluecube_Cube_mesh_layer_1_tri_0[];
 extern Gfx mat_bluecube_f3dlite_material_001[];
+extern Gfx mat_revert_bluecube_f3dlite_material_001[];
 extern Gfx bluecube_Cube_mesh_layer_1[];
