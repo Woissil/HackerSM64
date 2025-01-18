e611b792e28baeb014cb2a651dff97ad8c764683
NinteNerdTheGoat <nintenerdthegoat@gmail.com>
2025-01-18 16:14:07 +0100

    c4 + wc fix

--- levels/ccm/area_1/geo.inc.c
+++ levels/ccm/area_1/geo.inc.c
@@ -35,7 +35,7 @@ const GeoLayout ccm_area_1[] = {
 				GEO_OPEN_NODE(),
 					GEO_BRANCH(1, ccm_area_1_geo),
 					GEO_RENDER_OBJ(),
-					GEO_ASM(ENVFX_MODE_NONE, geo_envfx_main),
+					GEO_ASM(ENVFX_SNOW_NORMAL, geo_envfx_main),
 				GEO_CLOSE_NODE(),
 			GEO_CLOSE_NODE(),
 		GEO_CLOSE_NODE(),
